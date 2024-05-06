ab747aa9f592737fef325459ecca34299f1f37bc
Easton Archer <ea02archer@gmail.com>
2024-05-06 14:03:31 -0500

    Update index.js

--- index.js
+++ index.js
@@ -89,4 +89,3 @@ const resImage = await client.graphql.get()
 // Extract the result image and write it to the filesystem
 const result = resImage.data.Get.Image[0].image;
 writeFileSync('./result.jpg', result, 'base64');
-
