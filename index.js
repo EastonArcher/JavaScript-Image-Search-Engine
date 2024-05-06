42029f4ae5ea8750510133c07021af80c0146738
Easton Archer <ea02archer@gmail.com>
2024-05-06 14:02:54 -0500

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const client = weaviate.client({
     host: 'localhost:8080',
 });
 
-// Why are docker & weave not connecting??
+// Why are docker & weave not connecting???
 
 // Fetch current schema from Weaviate and then logging it
 const schemaRes = await client.schema.getter().do();
