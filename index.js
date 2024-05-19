e0fe36968df0ef07e8debf9ac37aee7f4b7950fc
Easton Archer <ea02archer@gmail.com>
2024-05-19 18:38:33 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ import { readFileSync, readdirSync, writeFileSync } from 'fs';
 import weaviate from 'weaviate-ts-client';
 
 
-
 // Setting up Weaviate client
 const client = weaviate.client({
     scheme: 'http',
